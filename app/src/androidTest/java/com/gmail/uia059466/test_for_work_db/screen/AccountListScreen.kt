5f3101e39af2340d20c0b778d8669d3a1822605c
Serhi <pt31sh9@protonmail.com>
2021-04-07 00:25:48 +0700

    test: метод обновлен, чтобы устройство ждало пока отобразиться следующий экран

--- app/src/androidTest/java/com/gmail/uia059466/test_for_work_db/screen/AccountListScreen.kt
+++ app/src/androidTest/java/com/gmail/uia059466/test_for_work_db/screen/AccountListScreen.kt
@@ -6,34 +6,40 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.uiautomator.UiDevice
-import com.gmail.uia059466.test_for_work_db.utils.BaseScreen
 import com.gmail.uia059466.test_for_work_db.R
+import com.gmail.uia059466.test_for_work_db.utils.BaseScreen
 import com.gmail.uia059466.test_for_work_db.utils.TestUtils
 import com.gmail.uia059466.test_for_work_db.utils.TestUtils.withRecyclerView
 
 class AccountListScreen(on: UiDevice) : BaseScreen(on) {
-    
-    override val contentLayout: Int= R.id.accounts_list_content
+
+    override val contentLayout: Int = R.id.accounts_list_content
     private val list = R.id.list
 
     override fun checkDefaultLayout() {
         checkViewDisplayed(contentLayout)
     }
 
-    fun checkTitle(title:String){
+    fun checkTitle(title: String) {
         Espresso.onView(withId(R.id.toolbar))
             .check(ViewAssertions.matches(ViewMatchers.hasDescendant(ViewMatchers.withText(title))))
     }
 
-    fun checkSizeRecycler(size:Int)  {
+    fun checkSizeRecycler(size: Int) {
         Espresso.onView(withId(list))
             .check(ViewAssertions.matches(ViewMatchers.hasChildCount(size)))
     }
-    fun clickOnList(position: Int) {
-        Espresso.onView(withRecyclerView(list).atPosition(position)).perform(ViewActions.click());    }
 
 
-    fun checkItemView(position: Int, title: String, amount:String) {
+    fun clickOnList(position: Int): AccountDisplayScreen {
+        Espresso.onView(withRecyclerView(list).atPosition(position)).perform(ViewActions.click())
+        val new=AccountDisplayScreen(device)
+        new.waitForScreenToBeDisplayed()
+        return new
+    }
+
+
+    fun checkItemView(position: Int, title: String, amount: String) {
         Espresso.onView(
             TestUtils.withRecyclerView(list).atPositionOnView(position, R.id.title_account_tv)
         ).check(
@@ -49,9 +55,8 @@ class AccountListScreen(on: UiDevice) : BaseScreen(on) {
 
     fun openAddAccount(): AddEditAccountScreen {
         Espresso.onView(withId(R.id.menu_add)).perform(ViewActions.click())
-        val new=AddEditAccountScreen(device)
+        val new = AddEditAccountScreen(device)
         new.waitForScreenToBeDisplayed()
         return new
     }
-
 }
